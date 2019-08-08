ce2babd0ee942c522aeb5590f040218192f94df3
owen <owen@oms.sh>
2019-08-08 02:14:57 -0400

    Highlight current row

--- src/App.js
+++ src/App.js
@@ -34,7 +34,10 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
 
+    // constants
     this.num_stages = 7;
+
+    // state
     this.state = {
       row_active     : Grid.Row.TOP,
       side_highlight : null,    // Grid.Column
@@ -46,27 +49,42 @@ class Board extends React.Component {
         SE : "Not punk",
       },
     };
-  }
 
-  renderLabel(cell) { // e.g. 'NW'
-    const is_loud = (cell) => {
-      const idxLoud = this.state.row_active * this.state.side_highlight;
-      return Grid.Cell[cell] === idxLoud;
-    };
+    // register keypresses
+    const board = this;
+    window.addEventListener('keydown', function(e) {
+      if (e.metaKey || e.altKey || e.ctrlKey) { return; }
+      else if (e.code === "KeyQ") { board.selectSide(Grid.Column.LEFT);  }
+      else if (e.code === "KeyP") { board.selectSide(Grid.Column.RIGHT); }
+    });
+    window.addEventListener('keyup', (_) => board.highlightSide(null) );
 
-    return (
-      <Label
-        name={ this.state.labels[cell] }
-        className={ is_loud(cell) ? "labelLoud" : "labelQuiet" }
-      />
-    );
   }
 
   highlightSide(side) { // Grid.Column
     let newstate = this.state;
     newstate.side_highlight = side;
-    if (side != null) { newstate.row_active = (newstate.row_active === 3) ? 5 : 3; } // just for testing
+    if (side != null) {
+      // just for testing
+      newstate.row_active = (newstate.row_active === 3) ? 5 : 3;
+    }
     this.setState(newstate);
+  }
+
+  selectSide(side) { // Grid.Column
+    this.highlightSide(side);
+    console.log(" select " + side );
+  }
+
+  renderLabel(cell) { // e.g. 'NW'
+    const idxLoud = this.state.row_active * this.state.side_highlight;
+    const isLoud = Grid.Cell[cell] === idxLoud;
+    return (
+      <Label
+        name={ this.state.labels[cell] }
+        className={ isLoud ? "labelLoud" : "labelQuiet" }
+      />
+    );
   }
 
   renderButton(column, label) { // Grid.Column
@@ -76,7 +94,7 @@ class Board extends React.Component {
         <button
           className="button"
           id={id}
-          onMouseDown={ () => this.highlightSide(column) }
+          onMouseDown={ () => this.selectSide(column) }
           onMouseUp={   () => this.highlightSide(null)   }
         >
         Press '{label}'
@@ -85,7 +103,10 @@ class Board extends React.Component {
     );
   }
 
+  //const circleTop = this.state.row_active === Grid.Row.TOP;
+  //const circleBottom = this.state.row_active === Grid.Row.TOP;
   render() {
+    const circleRow = (row) => this.state.row_active === row;
     return (
       <div>
 
@@ -96,11 +117,11 @@ class Board extends React.Component {
       <table className="gameboard">
       <tbody>
 
-        <tr>
+        <tr className={ circleRow(Grid.Row.TOP) && "circled" }>
           { this.renderLabel( Grid.Names.NW ) }
           { this.renderLabel( Grid.Names.NE ) }
         </tr>
-        <tr>
+        <tr className={ circleRow(Grid.Row.BOTTOM) && "circled" }>
           { this.renderLabel( Grid.Names.SW ) }
           { this.renderLabel( Grid.Names.SE ) }
         </tr>
